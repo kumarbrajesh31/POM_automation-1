2e1b940dd6e903d53e640effbee9cc77112c0a5a
umeshmit3 <umesh.mit3@gmail.com>
2019-07-20 19:37:26 +0530

    commit message

---
+++ src/test/java/com/practice/qa/scripts/NewScr_added.java
@@ -0,0 +1,26 @@
+package com.practice.qa.scripts;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class NewScr {
+	
+	public static WebDriver driver;
+	
+	@BeforeMethod
+	public void envSetUp()
+	{
+	System.setProperty("webdriver.chrome.driver", "D:\\software\\selenium_software\\Seleniumdrivers\\chromedriver.exe");
+	 driver=new ChromeDriver();
+	
+	}
+	
+	//Test
+	@Test
+	public void testLogin (){
+//		driver.navigateTo("http://dev.shortdatefood.com")
+		System.out.println("Test");
+	}
+}
