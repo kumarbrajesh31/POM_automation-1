18646d9f68a82a2aed646754ddfa8f26a1a2e052
Brajesh <kumarbrajesh31@gmail.com>
2020-05-03 18:33:04 +0530

    1st commit form User 2

---
+++ Auto/src/test/java/com/practice/qa/scripts/GitDemo.java
@@ -0,0 +1,10 @@
+package com.practice.qa.scripts;
+
+public class GitDemo {
+public static void main(String[] args) {
+	System.out.println("This is git demo fine");
+	System.out.println("user2");
+	System.out.println("user2_1");
+	System.out.println("user2_2");
+}
+}
